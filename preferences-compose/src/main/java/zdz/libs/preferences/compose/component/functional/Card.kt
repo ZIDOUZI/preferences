496383dfb0d98f5d1b833670082ff78bbf0aa5d5
ZIDOUZI <1662093634@qq.com>
2023-10-20 14:38:01 +0800

    Update Card.kt

--- preferences-compose/src/main/java/zdz/libs/preferences/compose/component/functional/Card.kt
+++ preferences-compose/src/main/java/zdz/libs/preferences/compose/component/functional/Card.kt
@@ -9,17 +9,25 @@ import zdz.libs.preferences.compose.contracts.PreferenceGroupScope
 @Composable
 fun PreferenceGroupScope.Card(
     title: String,
+    onClick: () -> Unit,
     modifier: Modifier = Modifier,
     summary: String? = null,
     enabled: Boolean = true,
-    onClick: () -> Unit = {},
+    titlePresent: @Composable (() -> Unit)? = null,
+    summaryPresent: @Composable (() -> Unit)? = null,
     icon: @Composable (() -> Unit)? = null,
+    info: @Composable (() -> Unit)? = null,
     trailing: @Composable (() -> Unit)? = null,
+    elevation: Dp = 2.dp,
 ) = Base(
     title = title,
     modifier = modifier.clickable(onClick = onClick),
     summary = summary,
     enabled = enabled,
+    titlePresent = titlePresent,
+    summaryPresent = summaryPresent,
     icon = icon,
+    info = info,
     trailing = trailing,
+    elevation = elevation
 )
